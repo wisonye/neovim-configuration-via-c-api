d988c66fefb635c2777a976125578b46de3392cf
Wison Ye <wisonye@gmail.com>
2025-12-12 17:13:59 +1300

    [ master ] - project_command: fixed module state and picker the default one when user selected nothing.

--- src/project_command.rs
+++ src/project_command.rs
@@ -20,15 +20,24 @@
 //!  );
 //! ```
 
-#[derive(Debug, Default)]
-struct MyProjectCommandState {
+#[derive(Debug)]
+struct ProjectCommandState {
+    cmd_list: Vec<String>,
     //
-    // project_dir <--> command list
+    // The default selection index from the `cmd_list`
     //
-    cmd_map: HashMap<String, Vec<String>>,
+    default_cmd_index: Option<usize>,
 }
 
-impl MyProjectCommandState {
+#[derive(Debug, Default)]
+struct ModuleState {
+    //
+    // project_dir <--> project command state
+    //
+    cmd_map: HashMap<String, ProjectCommandState>,
+}
+
+impl ModuleState {
     fn init() -> Self {
         Self {
             cmd_map: HashMap::with_capacity(10),
@@ -39,8 +48,8 @@ impl MyProjectCommandState {
 ///
 /// Private module-scope state
 ///
-static MY_PROJECT_COMMAND_STATE: LazyLock<Mutex<MyProjectCommandState>> =
-    LazyLock::new(|| Mutex::new(MyProjectCommandState::init()));
+static MY_PROJECT_COMMAND_STATE: LazyLock<Mutex<ModuleState>> =
+    LazyLock::new(|| Mutex::new(ModuleState::init()));
 
 ///
 /// Get back all `*.sh` files in the current project directory
@@ -68,7 +77,7 @@ fn get_project_script_files(project_dir: &str) -> std::io::Result<Vec<String>> {
 ///
 ///
 ///
-fn picker_selected_callback(project_dir: &str, cmd: &String) {
+fn picker_selected_callback(project_dir: &str, selected_cmd: String) {
     #[cfg(feature = "enable_project_command_debug_print")]
     const LOGGER_PREFIX: &'static str = "[ project_command - picker_selected_callback ]";
 
@@ -76,25 +85,60 @@ fn picker_selected_callback(project_dir: &str, cmd: &String) {
     // Lock the state and get back the cmd list
     //
     let mut locked_state = MY_PROJECT_COMMAND_STATE.lock();
-    let state = locked_state.as_mut().unwrap();
+    let module_state = locked_state.as_mut().unwrap();
 
-    if let Some(cmd_list) = state.cmd_map.get_mut(project_dir) {
+    if let Some(state) = module_state.cmd_map.get_mut(project_dir) {
+        let mut cmd = selected_cmd;
+
+        //
+        // Pick the first line from the `state.cmd_list` if `cmd` is empty, otherwise, exit
+        //
+        if cmd.len() == 0 {
+            if state.cmd_list.len() == 0 {
+                return;
+            }
+
+            if let Some(cmd_index) = state.default_cmd_index {
+                cmd = state.cmd_list[cmd_index].clone();
+            } else {
+                cmd = state.cmd_list[0].clone();
+            }
+        }
+
+        #[cfg(feature = "enable_project_command_debug_print")]
+        nvim::print!("{LOGGER_PREFIX} called with '{cmd}'.");
+
         //
         // Update the cmd list if the cmd doesn't exists
         //
-        if cmd_list
+        if state
+            .cmd_list
             .iter()
-            .find(|&item| item.cmp(cmd) == core::cmp::Ordering::Equal)
+            .find(|&item| item.cmp(&cmd) == core::cmp::Ordering::Equal)
             .iter()
             .count()
             == 0
         {
-            cmd_list.push(cmd.clone());
+            state.cmd_list.push(cmd.clone());
 
             //
             // Remove the empty placeholder line (used for rendering the empty list window) if exists.
             //
-            cmd_list.retain(|line| !line.is_empty());
+            state.cmd_list.retain(|line| !line.is_empty());
+        }
+
+        //
+        // Update the `default_cmd_index`
+        //
+        for index in 0..state.cmd_list.len() {
+            if state.cmd_list[index].cmp(&cmd) == core::cmp::Ordering::Equal {
+                state.default_cmd_index = Some(index);
+
+                #[cfg(feature = "enable_project_command_debug_print")]
+                nvim::print!("{LOGGER_PREFIX} update 'default_cmd_index' to: {index}");
+
+                break;
+            }
         }
     }
 }
@@ -134,11 +178,11 @@ fn open(options: ProjectCommandOptions) {
     //
     {
         let mut locked_state = MY_PROJECT_COMMAND_STATE.lock();
-        let state = locked_state.as_mut().unwrap();
+        let module_state = locked_state.as_mut().unwrap();
         //
         // Only init the cmd list when it doesn't exists.
         //
-        if state.cmd_map.get(&project_dir).is_none() {
+        if module_state.cmd_map.get(&project_dir).is_none() {
             if options.enable_script_files {
                 if let Ok(script_file_list) = get_project_script_files(&project_dir) {
                     let mut cmd_list = Vec::with_capacity(script_file_list.len());
@@ -146,16 +190,26 @@ fn open(options: ProjectCommandOptions) {
                         cmd_list.push(script_file);
                     }
 
-                    state.cmd_map.insert(project_dir.to_string(), cmd_list);
+                    module_state.cmd_map.insert(
+                        project_dir.to_string(),
+                        ProjectCommandState {
+                            cmd_list,
+                            default_cmd_index: None,
+                        },
+                    );
                 }
             } else {
-                state
-                    .cmd_map
-                    .insert(project_dir.to_string(), Vec::with_capacity(5));
+                module_state.cmd_map.insert(
+                    project_dir.to_string(),
+                    ProjectCommandState {
+                        cmd_list: Vec::with_capacity(5),
+                        default_cmd_index: None,
+                    },
+                );
             }
 
             #[cfg(feature = "enable_project_command_debug_print")]
-            nvim::print!("{LOGGER_PREFIX} state: {state:#?}");
+            nvim::print!("{LOGGER_PREFIX} state: {module_state:#?}");
         }
     }
 
@@ -163,16 +217,44 @@ fn open(options: ProjectCommandOptions) {
     // Lock the state
     //
     let mut locked_state = MY_PROJECT_COMMAND_STATE.lock();
-    let state = locked_state.as_mut().unwrap();
+    let module_state = locked_state.as_mut().unwrap();
 
     //
     // Get back the `project_dir` cmd list
     //
-    // if let Some(cmd_list) = state.cmd_map.get_mut(&project_dir) {
-    if let Some(cmd_list) = state.cmd_map.get(&project_dir) {
+    if let Some(state) = module_state.cmd_map.get(&project_dir) {
+        let cmd_list_len = state.cmd_list.len();
+        let mut temp_cmd_list =
+            Vec::<String>::with_capacity(if cmd_list_len > 0 { cmd_list_len } else { 1 });
+
+        //
+        // If `state.default_cmd_index != None` then put the `default_cmd` on top then follow by the rest
+        //
+        let display_cmd_list: &Vec<String> = if state.default_cmd_index.is_none() {
+            &state.cmd_list
+        } else {
+            // Put the default cmd on top
+            let top_line = state.cmd_list[state.default_cmd_index.unwrap()].clone();
+            temp_cmd_list.push(top_line.clone());
+
+            // Copy the reset
+            for line in &state.cmd_list {
+                if line != &top_line {
+                    temp_cmd_list.push(line.clone());
+                }
+            }
+
+            // return the re-ordered cmd list
+            &temp_cmd_list
+        };
+
+        //
+        // Open the picker
+        //
         let open_result = create_editable_picker_with_options(
             &mut EditablePickerOptions {
-                title: "Project Command ('Ctrl+d' to delete item, 'Ctrl+e' to close picker)".to_string(),
+                title: "Project Command ('Ctrl+d' to delete item, 'Ctrl+e' to close picker)"
+                    .to_string(),
                 window_opts: PopupWindowOptions {
                     border: WindowBorder::Rounded,
                     window_width_ratio: None,
@@ -181,10 +263,10 @@ fn open(options: ProjectCommandOptions) {
                     auto_height: true,
                     buffer: None,
                 },
-                list: cmd_list,
+                list: &display_cmd_list,
             },
             move |selected_text: String| {
-                picker_selected_callback(&project_dir, &selected_text);
+                picker_selected_callback(&project_dir, selected_text);
             },
         );
 
